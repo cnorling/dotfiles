0c9739fb3fe94ba77602e89c3c9a62bd58e9fdf6
salineselin <salineselin@gmail.com>
2022-07-21 21:54:21 -0600

    bound installation steps to idempotent codeblocks

--- setup.sh
+++ setup.sh
@@ -2,32 +2,46 @@
 # /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/salineselin/dotfiles/master/setup.sh)"
 
 # install brew then add it to PATH until .sharedrc.sh can do it
-if ! [ -d $HOME/linuxbrew/.linuxbrew/bin ]
+if ! [ -d home/linuxbrew/.linuxbrew/bin ]
     then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
 fi
 
 # install jq
-brew install jq
+if ! [ -f home/linuxbrew/.linuxbrew/bin/jq ]
+    then brew install jq
+fi
 
 # install zsh
-brew install zsh
+if ! [ -f home/linuxbrew/.linuxbrew/bin/zsh ]
+    then brew install zsh
+fi
 
 # install oh-my-zsh
-/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if ! [ -f $HOME/.oh-my-zsh/oh-my-zsh.sh ]
+    then /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # install zsh autosuggestions 
 # source: https://dev.to/pratik_kale/customise-your-terminal-using-zsh-powerlevel10k-1og5
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/zsh-autosuggestions
+if ! [ -d ~/.oh-my-zsh/zsh-autosuggestions ]
+    then git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/zsh-autosuggestions
+fi
 
 # install zsh syntax highlighting
 # source: https://dev.to/pratik_kale/customise-your-terminal-using-zsh-powerlevel10k-1og5
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
+if ! [ -d ~/.oh-my-zsh/zsh-syntax-highlighting ]
+    then git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
+fi
 
 # install powerlevel10k
 # maybe run this command after if you're setting up for the first time
 # echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+if ! [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]
+    then git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+fi
 
-# run bash_aliases
-/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/salineselin/dotfiles/master/.bash_aliases)"
+# run sharedrc.sh for init
+if ! [ -f ~/.sharedrc.sh ]
+    then /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/salineselin/dotfiles/master/.sharedrc.sh)"
+fi
