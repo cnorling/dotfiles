399a2c08170fdc44f067ae1bf25f3bf73e830377
cerise norling <cnorling@mychg.com>
2022-07-22 12:20:28 -0600

    added logging for each step

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 
 # install brew then add it to PATH until .sharedrc.sh can do it
 if ! [ -d /home/linuxbrew/.linuxbrew/bin ]
-    then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    then echo "brew is missing, installing" 
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # always add brew to path, it cant hurt
@@ -11,52 +12,64 @@ export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
 
 # install jq
 if ! [ -f /home/linuxbrew/.linuxbrew/bin/jq ]
-    then brew install jq
+    then echo "jq is missing, installing"
+    brew install jq
 fi
 
 # install zsh
 if ! [ -f /home/linuxbrew/.linuxbrew/bin/zsh ]
-    then brew install zsh
+    then echo "zsh is missing, installing"
+    brew install zsh
 fi
 
 # install oh-my-zsh
 if ! [ -f ~/.oh-my-zsh/oh-my-zsh.sh ]
-    then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    then echo "oh-my-zsh is missing, installing"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # install zsh autosuggestions 
 # source: https://dev.to/pratik_kale/customise-your-terminal-using-zsh-powerlevel10k-1og5
 if ! [ -d ~/.oh-my-zsh/zsh-autosuggestions ]
-    then git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/zsh-autosuggestions
+    then echo "zsh-autosuggestions is missing, installing"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/zsh-autosuggestions
 fi
 
 # install zsh syntax highlighting
 # source: https://dev.to/pratik_kale/customise-your-terminal-using-zsh-powerlevel10k-1og5
 if ! [ -d ~/.oh-my-zsh/zsh-syntax-highlighting ]
-    then git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
+    then echo "zsh-syntax-highlighting is missing, installing"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
 fi
 
 # install powerlevel10k
 if ! [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]
-    then git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+    then echo "powerlevel10k is missing, installing"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 fi
 
 # run sharedrc.sh for init
 if ! [ -f ~/.sharedrc.sh ]
-    then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/salineselin/dotfiles/master/.sharedrc.sh)"
+    then echo ".sharedrc.sh has not yet been initialized, running"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/salineselin/dotfiles/master/.sharedrc.sh)"
 fi
 
 # install podman
 if ! [ -f /usr/bin/podman ]
-    then sudo apt-get install podman
+    then "podman is missing, installing"
+    sudo apt-get install podman
 fi
 
 # install tfenv
 if ! [ -f /home/linuxbrew/.linuxbrew/bin/tfenv ]
-    then brew install tfenv
+    then "tfenv is missing, installing"
+    brew install tfenv
 fi
 
 # install terraform-docs
 if ! [ -f /home/linuxbrew/.linuxbrew/bin/terraform-docs ]
-    then brew install terraform-docs
+    then "terraform-docs is missing, installing"
+    brew install terraform-docs
 fi
+
+echo "profile is all setup!"
