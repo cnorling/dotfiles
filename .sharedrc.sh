c810bc02cf1ed10dff355100c8573a9d8e4dcbfc
cerise norling <salineselin@gmail.com>
2023-02-05 01:18:05 -0700

    updated sharedrc to target a different envrc

--- .sharedrc.sh
+++ .sharedrc.sh
@@ -150,8 +150,8 @@ if [ -f '/home/cnorling/google-cloud-sdk/path.zsh.inc' ]; then . '/home/cnorling
 if [ -f '/home/cnorling/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/cnorling/google-cloud-sdk/completion.zsh.inc'; fi
 
 # perform local only operations
-if [ -f '/home/cnorling/.envrc' ]; then
-    . '/home/cnorling/.envrc'
+if [ -f '/home/cnorling/.envrc.sh' ]; then
+    . '/home/cnorling/.envrc.sh'
 fi
 
 bgdc
